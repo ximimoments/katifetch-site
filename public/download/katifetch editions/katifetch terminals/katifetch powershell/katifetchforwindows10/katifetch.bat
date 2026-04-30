a4e7e032e0ae36cc8faccdf02cebaa6f43b10944
ximimoments <161499192+ximimoments@users.noreply.github.com>
2026-04-30 15:52:35 -0300

    Delete public/download/katifetch editions directory

--- public/download/katifetch editions/katifetch terminals/katifetch powershell/katifetchforwindows10/katifetch.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-powershell -ExecutionPolicy Bypass -NoProfile -File "%USERPROFILE%\Katifetch-Windows10\katifetch-windows10.ps1" %*
--- public/download/katifetch editions/katifetch terminals/katifetch powershell/katifetchforwindows1121h2/katifetch.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-powershell -ExecutionPolicy Bypass -NoProfile -File "%USERPROFILE%\Downloads\katifetch-main\katifetch-main\katifetchforwindows1121h2\katifetchforwindows1121h2.ps1" %*
