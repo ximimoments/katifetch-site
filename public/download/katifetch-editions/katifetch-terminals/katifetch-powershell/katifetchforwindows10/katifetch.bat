67e1fd4ba98361da859e8141d01c1318880ebd73
ximimoments <161499192+ximimoments@users.noreply.github.com>
2026-04-30 16:06:48 -0300

    Add files via upload

---
+++ public/download/katifetch-editions/katifetch-terminals/katifetch-powershell/katifetchforwindows10/katifetch.bat
@@ -0,0 +1,2 @@
+@echo off
+powershell -ExecutionPolicy Bypass -NoProfile -File "%USERPROFILE%\Katifetch-Windows10\katifetch-windows10.ps1" %*
---
+++ public/download/katifetch-editions/katifetch-terminals/katifetch-powershell/katifetchforwindows1121h2/katifetch.bat
@@ -0,0 +1,2 @@
+@echo off
+powershell -ExecutionPolicy Bypass -NoProfile -File "%USERPROFILE%\Downloads\katifetch-main\katifetch-main\katifetchforwindows1121h2\katifetchforwindows1121h2.ps1" %*
